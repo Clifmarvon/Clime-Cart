60d4229344cefabb7b35e6bffc0fd32708e3ea4f
Clifmarvon <127095593+Clifmarvon@users.noreply.github.com>
2023-07-29 20:43:17 +0000

    jscre

--- cart.js
+++ cart.js
@@ -1,66 +1,61 @@
 // change navbar styles on scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 2)
-})
-
-
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 2);
+});
 
 //show/hide faq answer
 
-const faqs = document.querySelectorAll('.faq')
+const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-   
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
 
-//change icon when faq is toggled
-const icon = faq.querySelector('.faq__icon i');
-if(icon.className === 'uil uil-plus') {
-    icon.className = "uil uil-minus"
-} else {
-    icon.className = "uil uil-plus"
-}
-
-})
-})
-
+    //change icon when faq is toggled
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
 
 //show /hide nav menu
-const menu =document.querySelector(".nav__menu");
-const menuBtn =document.querySelector("#open-menu-btn");
-const closeBtn =document.querySelector("#close-menu-btn");
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
 
-
-menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
-})
-
-
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
 
 /////close nav menu
 
 const closeNav = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
-}
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
 
-closeBtn.addEventListener('click', closeNav);
- 
+closeBtn.addEventListener("click", closeNav);
 
+const arrUP = document.querySelector(".uil-arrow-circle-up");
 
+window.addEventListener("scroll", () => {
+  var winds = window.scrollY;
 
-const arrUP = document.querySelector('.uil-arrow-circle-up');
-
-let winds = Window.scrollY;
-
-if (winds >= 550 ){
-    arrUP.style.display = 'block';
-}else{
-    arrUP.style.display = 'none';
-}
+  if (winds >= 520) {
+    arrUP.style.display = "block";
+  } else {
+    arrUP.style.display = "none";
+  }
+});
+arrUp.addEventListener("click", () => {
+  winds = 1;
+});
